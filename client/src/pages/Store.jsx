d2290f20da650c7534bc5c2a7414a9677ad9774d
geenathWeerasingha <73776728+geenathWeerasingha@users.noreply.github.com>
2023-07-15 21:46:11 +0530

    commit1

---
+++ client/src/pages/Store.jsx
@@ -0,0 +1,10 @@
+import StandardLayout from "../components/layout/StandardLayout";
+
+
+function Store() {
+  return (
+      <StandardLayout>Store</StandardLayout>
+  );
+}  
+
+export default Store
