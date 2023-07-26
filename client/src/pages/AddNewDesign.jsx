803ae485949417d76a7b07bc97d2d7fe3218ea73
Devin Yapa <72931352+Devinyapa@users.noreply.github.com>
2023-07-26 10:31:03 +0530

    do some changes

--- client/src/pages/AddNewDesign.jsx
+++ client/src/pages/AddNewDesign.jsx
@@ -114,7 +114,7 @@ const AddNewDesignPage = () => {
 
           <div className="flex justify-end">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-lg"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-lg mt-3"
               type="submit"
             >
               Submit Design
