001098eec39545d627ad9817af0755e14e999047
Janod-Umayanga <87465214+Janod-Umayanga@users.noreply.github.com>
2023-08-06 19:25:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,4 +38,8 @@ app.use('/login', loginModel);
 
 app.use('/logout', logoutRoutes);
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Artizon Backend' });
+});
+
 app.listen(process.env.PORT, () => console.log('Server is running on port ' + process.env.PORT));
