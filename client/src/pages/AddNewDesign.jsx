e29fa1522af25d6275c66785599900a1cca3acf5
Devin Yapa <72931352+Devinyapa@users.noreply.github.com>
2023-08-07 19:59:28 +0530

    do back end of design upload

--- client/src/pages/AddNewDesign.jsx
+++ client/src/pages/AddNewDesign.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
-import { AiOutlineDelete, AiOutlineUpload } from 'react-icons/ai';
-import StandardLayout from '../components/layout/StandardLayout';
+
+import React, { useState } from "react";
+import { AiOutlineDelete, AiOutlineUpload } from "react-icons/ai";
+import StandardLayout from "../components/layout/StandardLayout";
+import axios from "axios";
 
 const AddNewDesignPage = () => {
+  const materialOptions = ["Cotton", "Silk", "Linen", "Polyester", "Rayon"];
+  const [supportingMaterials, setSupportingMaterials] = useState([
+    { material: "" },
+  ]);
+  const [images, setImages] = useState([]);
   const [imagePreviews, setImagePreviews] = useState([]);
+  // const [file, setFile] = useState();
+  const [file, setFiles] = useState([]);
 
   const handleImageUpload = (event) => {
     const files = event.target.files;
     const fileArray = Array.from(files).slice(0, 3);
+    const newFiles = fileArray.map((file) => ({ file, name: file.name }));
+    setImages((prevFiles) => [...prevFiles, ...newFiles]);
+    
+    // setFile(event.target.files[0]);
+    setFiles(Array.from(files).slice(0, 3));
 
     Promise.all(
       fileArray.map((file) => {
@@ -26,6 +40,11 @@ const AddNewDesignPage = () => {
   };
 
   const handleImageDelete = (index) => {
+    setImages((prevFiles) => {
+      const newFiles = [...prevFiles];
+      newFiles.splice(index, 1);
+      return newFiles;
+    });
     setImagePreviews((prevPreviews) => {
       const newPreviews = [...prevPreviews];
       newPreviews.splice(index, 1);
@@ -33,96 +52,206 @@ const AddNewDesignPage = () => {
     });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Form submitted!');
+  const handleAddMaterial = () => {
+    if (supportingMaterials.length < 5) {
+      setSupportingMaterials((prevMaterials) => [
+        ...prevMaterials,
+        { material: "" }, // Add a new empty material when clicking "Add Material"
+      ]);
+    }
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("designName", event.target.designName.value);
+    formData.append("designDescription", event.target.designDescription.value);
+    formData.append("designerId", "1"); // Replace '123' with the actual designer ID
+
+    // Remove any empty materials from the formData
+    supportingMaterials.forEach((material, index) => {
+      formData.append(`materials[${index}].material`, material.material);
+    });
+
+ // Append the image file names to the formData
+      // formData.append('image', file);
+      for (let i = 0; i < file.length; i++) {
+        formData.append(`images[${i}]`, file[i]);
+      }
+  
+
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+   
+    console.log(formData.getAll("imagePreviews"));
+
+    axios
+      .post("http://localhost:3001/api/addNewDesign", formData, config)
+      .then((response) => {
+        console.log("Design and materials added successfully!", response.data);
+      })
+      .catch((error) => {
+        console.error("Error adding design and materials:", error);
+        // Handle the error and show an error message to the user
+      });
+  };
   return (
     <StandardLayout>
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">Add New Design</h1>
+      <div className="container mx-auto p-8">
+        <h1 className="text-3xl font-bold text-gray-800">Add New Design</h1>
 
-      <div className='px-1'>
-        <hr className="my-4 border-t-2 border-gray-300" />
-      </div>
-
-
-      <div className="mt-8">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="designName" className="block text-gray-800 font-semibold mb-2">
-              Design Name
-            </label>
-            <input
-              type="text"
-              id="designName"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-              placeholder="Enter design name"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="designDescription" className="block text-gray-800 font-semibold mb-2">
-              Design Description
-            </label>
-            <textarea
-              id="designDescription"
-              className="w-full px-4 py-2 border rounded-lg resize-none focus:outline-none focus:ring focus:border-blue-300"
-              rows="4"
-              placeholder="Enter design description"
-            ></textarea>
-          </div>
-
-          {/* Add other relevant data inputs for the design form */}
-
-          <div className="text-gray-800 font-semibold mb-2">
-          Upload Images (Up to 3)
+        <div className="px-1">
+          <hr className="my-4 border-t-2 border-gray-300" />
         </div>
-          
-          
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-    
-        {imagePreviews.map((preview, index) => (
-          <div key={index} className="relative">
-            <img src={preview} alt={`Design Preview ${index + 1}`} className="w-full h-48 object-cover rounded-lg" />
-            <button
-              className="absolute top-2 right-2 bg-red-500 hover:bg-red-700 text-white rounded-full p-2"
-              onClick={() => handleImageDelete(index)}
-            >
-              <AiOutlineDelete />
-            </button>
-          </div>
-        ))}
 
-        {imagePreviews.length < 3 && (
-          <div className="bg-gray-100 flex items-center justify-center rounded-lg">
-            <label htmlFor="upload" className="cursor-pointer">
-              <AiOutlineUpload className="text-4xl text-gray-400" />
-              <input
-                type="file"
-                id="upload"
-                className="sr-only"
-                accept="image/*"
-                multiple
-                onChange={handleImageUpload}
-              />
-            </label>
-          </div>
-        )}
-      </div>
+        <div className="flex justify-center">
+          <div className="w-full max-w-4xl bg-white p-6 rounded-lg shadow-md">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label
+                  htmlFor="designName"
+                  className="block text-gray-800 font-semibold mb-2"
+                >
+                  Design Name
+                </label>
+                <input
+                  type="text"
+                  id="designName"
+                  className="w-full px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Enter design name"
+                />
+              </div>
 
-          <div className="flex justify-end">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-lg mt-3"
-              type="submit"
-            >
-              Submit Design
-            </button>
+
+            <div className="mb-4">
+                <label
+                  htmlFor="designDescription"
+                  className="block text-gray-800 font-semibold mb-2"
+                >
+                  Design Description
+                </label>
+                <textarea
+                  id="designDescription"
+                  className="w-full px-4 py-3 border rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  rows="4"
+                  placeholder="Enter design description"
+                ></textarea>
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="designMaterials"
+                  className="block text-gray-800 font-semibold mb-2"
+                >
+                  Supporting Materials (Up to 5)
+                </label>
+                <div className="flex flex-col gap-4">
+                  {supportingMaterials.map((item, index) => (
+                    <div key={index} className="flex">
+                      <select
+                        className="w-full px-4 py-3 border bg-transparent rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        value={item.material}
+                        onChange={(e) => {
+                          const newMaterials = [...supportingMaterials];
+                          newMaterials[index].material = e.target.value;
+                          setSupportingMaterials(newMaterials);
+                        }}
+                      >
+                        <option value="">Select Material</option>
+                        {materialOptions.map((material) => (
+                          <option
+                            key={material}
+                            value={material}
+                            disabled={supportingMaterials.some(
+                              (item) =>
+                                item.material === material &&
+                                item.material !==
+                                  supportingMaterials[index].material
+                            )}
+                          >
+                            {material}
+                          </option>
+                        ))}
+                      </select>
+                      {index > 0 && (
+                        <button
+                          className="ml-2 bg-transparent hover:bg-red-600 hover:text-white text-gray-400 border rounded-lg px-3 py-2"
+                          onClick={() => {
+                            setSupportingMaterials((prevMaterials) => {
+                              const newMaterials = [...prevMaterials];
+                              newMaterials.splice(index, 1);
+                              return newMaterials;
+                            });
+                          }}
+                          type="button"
+                        >
+                          Remove
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                </div>
+                {supportingMaterials.length < 5 && (
+                  <button
+                    className="bg-transparent hover:bg-blue-500 hover:text-white text-gray-400 font-semibold px-4 py-2 rounded-lg mt-2 border"
+                    onClick={handleAddMaterial}
+                    type="button"
+                  >
+                    Add Material
+                  </button>
+                )}
+              </div>
+
+              <div className="text-gray-800 font-semibold mb-2">
+                Upload Images (Up to 3)
+              </div>
+
+<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                {imagePreviews.map((preview, index) => (
+                  <div key={index} className="relative">
+                    <img src={preview} alt={`Design Preview ${index + 1}`} className="w-full h-48 object-cover rounded-lg" />
+                    <button
+                      className="absolute top-2 right-2 bg-red-500 hover:bg-red-700 text-white rounded-full p-2"
+                      onClick={() => handleImageDelete(index)}
+                    >
+                      <AiOutlineDelete />
+                    </button>
+                  </div>
+                ))}
+
+                {imagePreviews.length < 3 && (
+                  <div className="bg-gray-100 flex items-center justify-center rounded-lg">
+                    <label htmlFor="upload" className="cursor-pointer">
+                      <AiOutlineUpload className="text-4xl text-gray-400" />
+                      <input
+                        type="file"
+                        id="upload"
+                        className="sr-only"
+                        accept="image/*"
+                        multiple
+                        onChange={handleImageUpload}
+                      />
+                    </label>
+                  </div>
+                )}
+              </div>
+
+              <div className="flex justify-end">
+                <button
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-3 rounded-lg mt-4"
+                  type="submit"
+                >
+                  Submit Design
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
     </StandardLayout>
   );
 };
--- client/src/pages/StylistDashboard.jsx
+++ client/src/pages/StylistDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { FaCheckCircle, FaTimesCircle, FaClipboardCheck } from 'react-icons/fa';
@@ -23,22 +23,22 @@ const DashboardLabel = ({ title, value, color }) => {
   const getIcon = (color) => {
     switch (color) {
       case 'blue':
-        return <FaCheckCircle className="text-5xl" />;
+        return <FaCheckCircle className="text-5xl text-blue-500" />;
       case 'red':
-        return <FaTimesCircle className="text-5xl" />;
+        return <FaTimesCircle className="text-5xl text-red-500" />;
       case 'yellow':
-        return <FaClipboardCheck className="text-5xl" />;
+        return <FaClipboardCheck className="text-5xl text-yellow-500" />;
       default:
         return null;
     }
   };
 
   return (
-    <div className={`bg-white rounded-lg shadow-lg p-8 flex items-center justify-center text-${color}-600`}>
+    <div className={`bg-${color}-100 rounded-lg shadow-lg p-4 flex items-center justify-center border border-${color}-300`}>
       {getIcon(color)}
       <div className="ml-4">
-        <h2 className="text-2xl font-semibold mb-2">{title}</h2>
-        <p className="text-4xl font-bold">{value}</p>
+        <h2 className="text-xl font-semibold text-gray-800 mb-2">{title}</h2>
+        <p className="text-3xl font-bold text-gray-900">{value}</p>
       </div>
     </div>
   );
@@ -46,16 +46,18 @@ const DashboardLabel = ({ title, value, color }) => {
 
 const DashboardCard = ({ title, link, icon, value }) => {
   return (
-    <div className="w-1/2 px-2">
+    <div className="w-full md:w-1/2 lg:w-1/3 p-2">
       <Link to={link}>
-        <div className="rounded-lg shadow-md overflow-hidden bg-gradient-to-br from-gray-500 to-gray-800 hover:shadow-lg cursor-default transition duration-300">
+        <div className="rounded-lg shadow-lg overflow-hidden bg-white hover:shadow-xl cursor-pointer transition duration-300">
           <div className="p-6">
-            <h2 className="text-2xl font-semibold text-white mb-4">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">
               {icon} {title}
             </h2>
             <div className="flex justify-between items-center">
-              <div className="text-white font-bold text-xl">{value}</div>
-              <div className="bg-white rounded-full p-3 text-blue-600 font-semibold text-sm cursor-pointer">View Details</div>
+              <div className="text-gray-900 font-bold text-xl">{value}</div>
+              <div className="bg-blue-500 rounded-full p-3 text-white font-semibold text-sm cursor-pointer hover:bg-blue-600 transition duration-300">
+                View Details
+              </div>
             </div>
           </div>
         </div>
@@ -63,7 +65,6 @@ const DashboardCard = ({ title, link, icon, value }) => {
     </div>
   );
 };
-
 
 const DashboardPage = () => {
   // Define your orders and other data here...
@@ -77,24 +78,22 @@ const DashboardPage = () => {
     <StandardLayout>
       <div className="container mx-auto p-8">
         <h1 className="text-3xl font-bold text-gray-800 mb-2">Dashboard</h1>
-         
-        <div className='px-2'>
-        <hr className="my-4 border-t-2 border-gray-200" />
-       </div>
+        <div className="px-2">
+          <hr className="my-4 border-t-2 border-gray-200" />
+        </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8 mt-6">
+        <div className="grid grid-cols-1  lg:grid-cols-3 gap-6 mb-8 mt-6">
+          
           <DashboardLabel title="Accepted Orders" value={acceptedOrders} color="blue" />
           <DashboardLabel title="Rejected Orders" value={rejectedOrders} color="red" />
           <DashboardLabel title="Orders in Approval" value={ordersInApproval} color="yellow" />
         </div>
 
-
-        <div className="flex justify-center mb-8">
-          <DashboardCard title="Orders" link="/order-status-update"  value={orders} />
-          <DashboardCard title="Review Orders" link="/review-order"  value={orderReview} />
+        <div className="flex flex-wrap justify-center mb-8 gap-4">
+          <DashboardCard title="Orders" link="/order-status-update" icon={<FaClipboardCheck className="text-5xl" />} value={orders} />
+          <DashboardCard title="Review Orders" link="/review-order" icon={<FaTimesCircle className="text-5xl" />} value={orderReview} />
           {/* Add more cards for other sections */}
         </div>
- 
 
         <div className="mt-8">
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">Orders Past 12 Months</h2>
