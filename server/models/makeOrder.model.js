b3b95de0e502e4baa6919793379f800cce56dbd5
Janod-Umayanga <87465214+Janod-Umayanga@users.noreply.github.com>
2023-08-13 14:21:35 +0530

    make order complete data insertion for the tables

--- server/models/makeOrder.model.js
+++ server/models/makeOrder.model.js
@@ -10,14 +10,35 @@ router.route('/').post((req, res) => {
 
     const VALUES1 = [req.body.material, req.body.color, req.body.note, req.body.logo, 'pending', req.body.customerId];
 
-    // const sql2 = 'INSERT INTO `login_details` (`email`, `usertype`, `password`) VALUES (?);';
-
     db.query(sql1, [VALUES1], (err, result) => {
         if (err) {
             console.error(err);
             return res.json({ Error: "Error_Insert_Data" });
         }
 
+        const quantities = req.body.quantities;
+        const sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+
+        const orderId = result.insertId;
+
+        for (let i = 0; i < quantities.length; i++) {
+            if (quantities[i]) {
+                const sql2 = 'INSERT INTO `order_quantity` (`size`, `quantity`, `tshirt_order_id`) VALUES (?);';
+
+                const VALUES2 = [sizes[i], quantities[i], orderId];
+
+                db.query(sql2, [VALUES2], (err, result) => {
+                    if (err) {
+                        console.error(err);
+                        return res.json({ Error: "Error_Insert_Data" });
+                    }
+                });
+            }
+        }
+
+        // console.log(quantities, quantities.length);
+        // console.log(orderId);
+        
         return res.json({ Status: "Success_Makeorder" });
     });
 });
